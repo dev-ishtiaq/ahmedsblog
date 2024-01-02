e9d2ca80c3b5254af6303759651a819cf4776254
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-01-02 22:33:38 +0600

    frontend rearraged

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.index');
+    return view('frontend.home');
+});
+Route::get('/about', function () {
+    return view('frontend.about');
+});
+Route::get('/blog', function () {
+    return view('frontend.blog');
+});
+Route::get('/blog_details', function () {
+    return view('frontend.blog_details');
+});
+Route::get('/contact', function () {
+    return view('frontend.contact');
 });
